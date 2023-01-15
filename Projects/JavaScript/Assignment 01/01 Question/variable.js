6ed2d7036eaa30e053895ec4705d2f0af1344ed6
venkatamadhav <venkat.madhav.98@gmail.com>
2023-01-15 14:21:07 +0530

    Added JavaScript Assignment

---
+++ Projects/JavaScript/Assignment 01/01 Question/variable.js
@@ -0,0 +1,9 @@
+let stringvariable = "FSJS 2.0";
+let booleanvariable = false;
+let undefinedvariable;
+let nullvariable = null;
+
+console.log("StringVariable is "+stringvariable+" and is of type "+typeof stringvariable);
+console.log("BooleanVariable is "+booleanvariable+" and is of type "+typeof booleanvariable);
+console.log("UndefinedVariable is "+undefinedvariable+" and is of type "+typeof undefinedvariable);
+console.log("NullVariable is "+nullvariable+" and is of type "+typeof nullvariable);
