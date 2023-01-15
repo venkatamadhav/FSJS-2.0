cda5776d5b1939b962e92c57ec173b19c3bcdf2e
venkatamadhav <venkat.madhav.98@gmail.com>
2023-01-15 14:23:00 +0530

    Added 02 Question

---
+++ Projects/JavaScript/Assignment 01/02 Question/variables.js
@@ -0,0 +1,7 @@
+let firstname = "Venkata";
+let lastname = "Madhav";
+let MaritalStatus = "Single";
+let Country = "India";
+let Age = "23";
+
+console.log(`My First Name is ${firstname} and last name is ${lastname}. My age is ${Age} and I reside in ${Country} and my marital status is ${MaritalStatus}`);
