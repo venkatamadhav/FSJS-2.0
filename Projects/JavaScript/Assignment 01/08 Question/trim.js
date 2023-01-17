bc946e45677e2a86485b744c58457ec1380f8765
venkatamadhav <venkat.madhav.98@gmail.com>
2023-01-17 23:52:29 +0530

    Added 08 Question

---
+++ Projects/JavaScript/Assignment 01/08 Question/trim.js
@@ -0,0 +1,9 @@
+let sentence ="     This sentence is to demonstrate how to use trim function to remove unneccessary spaces in a text                    "
+
+// In the below console log , it removes spaces from either ends.
+console.log(sentence.trim());
+
+// In the below console log , it removes spaces from start end.
+console.log(sentence.trimStart());
+// In the below console log , it removes spaces from last end.
+console.log(sentence.trimEnd());
