54c2704d38acd7ddaa3082393f3b93c8138b1166
venkatamadhav <venkat.madhav.98@gmail.com>
2023-01-18 09:12:36 +0530

    Added 09 Question

---
+++ Projects/JavaScript/Assignment 01/09 Question/boolean.js
@@ -0,0 +1,10 @@
+
+//Boolean Values to show truthy value
+console.log(10 > 3); 
+console.log("hello".length > 0); 
+console.log("hello"!=="bye"); 
+
+//Boolean Values to show Falsy values
+console.log(0 == true); 
+console.log(62 > 91); 
+console.log(undefined === null);
