1a1335bb80d45e4d61f33479247d10f1f7624218
venkatamadhav <venkat.madhav.98@gmail.com>
2023-01-19 22:17:11 +0530

    Added 10 Question

---
+++ Projects/JavaScript/Assignment 01/10 Question/comparisons.js
@@ -0,0 +1,25 @@
+let one = (4 > 3);
+let two = (4 >= 3);
+let three = (4 < 3);
+let four = (4 <= 3);
+let five = (4 == 4);
+let six = (4 === 4);
+let seven = (4 != 4);
+let eight = (4 !== 4);
+let nine = (4 != '4');
+let ten = (4 == '4');
+let eleven = (4 === '4');
+
+console.log(one == (4>3));
+console.log(two == (4 >= 3));
+console.log(three == (4 < 3));
+console.log(four == (4 <= 3));
+console.log(five == (4 == 4));
+console.log(six == (4 === 4));
+console.log(seven == (4 != 4));
+console.log(eight == (4 !== 4));
+console.log(nine == (4 != '4'));
+console.log(ten == (4 == '4'));
+console.log(eleven == (4 === '4'));
+
+console.log('python'.length !== 'jargon'.length);
