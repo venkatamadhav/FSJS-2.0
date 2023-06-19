91393b20eb8a39d03a6bf11542e561bd60f460fe
venkatamadhav <venkat.madhav.98@gmail.com>
2023-06-19 22:16:52 +0530

    Final assignment added

--- Projects/React/rock-paper-scissor/src/components/ActionIcon.js
+++ Projects/React/rock-paper-scissor/src/components/ActionIcon.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import {FaHandRock , FaHandPaper, FaHandScissors } from "react-icons/fa"
 
-function ActionIcon({action, ...props}){
-    const icons = {
-      rock: FaHandRock,
-      paper: FaHandPaper,
-      scissor: FaHandScissors,
-    };
-    const Icon = icons[action]
-    return (<Icon {...props}/>);
+// function ActionIcon({action, ...props}){
+//     const icons = {
+//       rock: FaHandRock,
+//       paper: FaHandPaper,
+//       scissor: FaHandScissors,
+//     };
+//     const Icon = icons[action]
+//     return (<Icon {...props}/>);
+// }
+ function ActionIcon({action, ...props}){
+  const _action = action.action;
+  if(_action === "rock"){
+    return <FaHandRock {...props}/>
+  }
+  else if (_action === "paper"){
+    return <FaHandPaper {...props}/>
+  }
+  else if (_action === "scissor"){
+    return <FaHandScissors {...props}/>
+  }
+  else {
+    return <FaHandRock {...props}/>
+  }
 }
 
 export default ActionIcon
